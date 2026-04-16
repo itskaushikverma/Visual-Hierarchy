566fba31e0925c7bb69533d2b992dead2219a6b5
Kaushik Verma <kaushikverma321@gmail.com>
2026-04-16 10:41:41 +0530

    Fix formatting of div element in App component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Flow from "./components/flow";
 export default function App() {
   return (
     <div className="max-h-screen">
-      <div className="m-3 border-2 border-white/20 rounded-lg shadow-md">
+      <div className="border-2 m-3 border-white/20 rounded-lg shadow-md">
         <ReactFlowProvider>
           <Flow />
         </ReactFlowProvider>
